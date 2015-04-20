753110cee4c4734f6f05c0ced599325c1969c6b0
Vinay Simha BN <vinaysimha@inforcecomputing.com>
2015-04-20 15:45:24 +0530

    WIP:dsi data working -truly display-flickering

--- drivers/gpu/drm/panel/panel-truly.c
+++ drivers/gpu/drm/panel/panel-truly.c
@@ -53,10 +53,190 @@ static inline struct panel_otm8018b *to_panel_otm8018b(struct drm_panel *panel)
         return container_of(panel, struct panel_otm8018b, panel);
 }
 
+static char pixels_on[1]={0x23};/* For testing , all pixel on*/
+
 static char enter_sleep[2] = {0x10, 0x00};
+/*************************** IC for AUO 4' MIPI * 480RGBx854************/
+static char write_memory1[4]={0xFF,0x80,0x09,0x01};//Enable EXTC
+static char write_memory2[2]={0x00,0x80};//Shift address
+static char write_memory3[3]={0xFF,0x80,0x09};	 //Enable Orise mode
+
+static char write_memory4[2]={0x00,0x00};
+static char write_memory5[2]={0xD8,0x87};//GVDD	4.8V
+static char write_memory6[2]={0x00,0x01};
+static char write_memory7[2]={0xD8,0x87};//NGVDD -4.8V
+static char write_memory8[2]={0x00,0xB1};
+static char write_memory9[2]={0xC5,0xA9};//[0]GVDD output Enable : 0xA9	VDD_18V=LVDSVDD=1.8V
+static char write_memory10[2]={0x00,0x91};
+static char write_memory11[2]={0xC5,0x79};//[7:4]VGH_level=15V [3:0]VGL_level=-12V
+static char write_memory12[2]={0x00,0x00};
+static char write_memory13[2]={0xD9,0x45};//VCOM=-1.15V
+
+static char write_memory14[2]={0x00,0x92};
+static char write_memory15[2]={0xC5,0x01};//pump45
+
+static char write_memory16[2]={0x00,0xA1};
+static char write_memory17[2]={0xC1,0x08};//reg_oscref_rgb_vs_video
+
+static char write_memory18[2]={0x00,0x81};
+static char write_memory19[2]={0xC1,0x66};//OSC Adj=65Hz
+
+static char write_memory20[2]={0x00,0xa3};
+static char write_memory21[2]={0xc0,0x1B};//source pch
+
+static char write_memory22[2]={0x00,0x82};
+static char write_memory23[2]={0xc5,0x83};//REG-Pump23 AVEE VCL
+
+static char write_memory24[2]={0x00,0x81};
+static char write_memory25[2]={0xc4,0x83};			 //source bias
+
+static char write_memory26[2]={0x00,0x90};
+static char write_memory27[2]={0xB3,0x02};			 //SW_GM 480X854
+
+static char write_memory29[2]={0x00,0x92};
+static char write_memory30[2]={0xB3,0x45};			 //Enable SW_GM
+
+static char write_memory31[2]={0x00,0xa0};
+static char write_memory32[2]={0xc1,0xea};
+
+static char write_memory33[2]={0x00,0xc0};
+static char write_memory34[2]={0xc5,0x00};
+
+static char write_memory35[2]={0x00,0x8b};
+static char write_memory36[2]={0xb0,0x40};
+
+static char write_memory37[2]={0x00,0x87};
+static char write_memory38[4]={0xC4,0x00,0x80,0x00};
+
+static char write_memory39[2]={0x00,0xB2};
+static char write_memory40[5]={0xF5,0x15,0x00,0x15,0x00}; //VRGH Disable
+
+static char write_memory41[2]={0x00,0x93};
+static char write_memory42[2]={0xC5,0x03}; //VRGH minimum
+///////////////////////////////////////////////////////////////////////////////
+static char write_memory43[2]={0x00,0xa7};
+static char write_memory44[2]={0xb3,0x01};          //panel_set[0]	= 1
+
+static char write_memory45[2]={0x00,0xa6};
+static char write_memory46[2]={0xb3,0x2b};          //reg_panel_zinv,reg_panel_zinv_pixel,reg_panel_zinv_odd,reg_panel_zigzag,reg_panel_zigzag_blue,reg_panel_zigzag_shift_r,reg_panel_zigzag_odd
+
+//C09x : mck_shift1/mck_shift2/mck_shift3
+static char write_memory47[2]={0x00,0x90};
+static char write_memory48[7]={0xC0,0x00,0x4E,0x00,0x00,0x00,0x03};
+
+//C0Ax : hs_shift/vs_shift
+
+static char write_memory49[2]={0x00,0xa6};
+static char write_memory50[4]={0xC1,0x01,0x00,0x00}; // 16'hc1a7 [7:0] : oscref_vedio_hs_shift[7:0]
+
+//Gamma2.2 +/-
+static char write_memory51[2]={0x00,0x00};
+static char write_memory52[17]={0xE1,0x05,0x0B,0x0F,0x0F,0x08,0x0D,0x0C,0x0B,0x02,0x06,0x16,0x12,0x18,0x24,0x17,0x00};
+//V255 V251 V247 V239 V231 V203 V175 V147 V108 V80  V52	 V24  V16  V8   V4   V0
+
+static char write_memory53[2]={0x00,0x00};
+static char write_memory54[17]={0xE2,0x05,0x0B,0x0F,0x0F,0x08,0x0D,0x0C,0x0B,0x02,0x06,0x16,0x12,0x18,0x24,0x17,0x00};
+//V255 V251 V247 V239 V231 V203 V175 V147 V108 V80  V52	 V24  V16  V8   V4   V0
+
+//--------------------------------------------------------------------------------
+//		initial setting 2 < tcon_goa_wave >
+//--------------------------------------------------------------------------------
+static char write_memory55[2]={0x00,0x91};         //zigzag reverse scan
+static char write_memory56[2]={0xB3,0x00};
+
+//CE8x : vst1, vst2, vst3, vst4
+static char write_memory57[2]={0x00, 0x80};
+static char write_memory58[13]={0xCE,0x85,0x01,0x18,0x84,0x01,0x18,0x00,0x00,0x00,0x00,0x00,0x00};
+
+//CE9x : vend1, vend2, vend3, vend4
+static char write_memory59[2]={0x00, 0x90};
+static char write_memory60[15]={0xCE,0x13,0x56,0x18,0x13,0x57,0x18,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+
+//CEAx : clka1, clka2
+static char write_memory61[2]={0x00, 0xa0};
+static char write_memory62[15]={0xCE,0x18,0x0B,0x03,0x5E,0x00,0x18,0x00,0x18,0x0A,0x03,0x5F,0x00,0x18,0x00};
+
+//CEBx : clka3, clka4
+static char write_memory63[2]={0x00, 0xb0};
+static char write_memory64[15]={0xCE,0x18,0x0D,0x03,0x5C,0x00,0x18,0x00,0x18,0x0C,0x03,0x5D,0x00,0x18,0x00};
+
+//CECx : clkb1, clkb2
+static char write_memory65[2]={0x00, 0xc0};
+static char write_memory66[15]={0xCE,0x38,0x0D,0x03,0x5E,0x00,0x10,0x07,0x38,0x0C,0x03,0x5F,0x00,0x10,0x07};
+
+//CEDx : clkb3, clkb4
+static char write_memory67[2]={0x00, 0xd0};
+static char write_memory68[15]={0xCE,0x38,0x09,0x03,0x5A,0x00,0x10,0x07,0x38,0x08,0x03,0x5B,0x00,0x10,0x07};
+
+//CFCx :
+static char write_memory69[2]={0x00, 0xC7};
+static char write_memory70[2]={0xCF, 0x04};
+
+static char write_memory71[2]={0x00, 0xC9};
+static char write_memory72[2]={0xCF, 0x00};
+
+//--------------------------------------------------------------------------------
+//		initial setting 3 < Panel setting >
+//--------------------------------------------------------------------------------
+// cbcx
+static char write_memory73[2]={0x00, 0xC0};
+static char write_memory74[2]={0xCB, 0x14};
+
+static char write_memory75[2]={0x00, 0xC2};
+static char write_memory76[6]={0xCB, 0x14,0x14,0x14,0x14,0x14};
+
+// cbdx
+static char write_memory77[2]={0x00, 0xD5};
+static char write_memory78[2]={0xCB, 0x14};
+
+static char write_memory79[2]={0x00, 0xD7};
+static char write_memory80[6]={0xCB, 0x14,0x14,0x14,0x14,0x14};
+
+// cc8x
+static char write_memory81[2]={0x00, 0x80};
+static char write_memory82[2]={0xCC, 0x01};
+
+static char write_memory83[2]={0x00, 0x82};
+static char write_memory84[6]={0xCC, 0x0F,0x0D,0x0B,0x09,0x05};
+
+// cc9x
+static char write_memory85[2]={0x00, 0x9A};
+static char write_memory86[2]={0xCC, 0x02};
+
+static char write_memory87[2]={0x00, 0x9C};
+static char write_memory88[4]={0xCC, 0x10,0x0E,0x0C};
+
+// ccax
+static char write_memory89[2]={0x00, 0xA0};
+static char write_memory90[2]={0xCC, 0x0A};
+
+static char write_memory91[2]={0x00, 0xA1};
+static char write_memory92[2]={0xCC, 0x06};
+
+// ccbx
+static char write_memory93[2]={0x00, 0xB0};
+static char write_memory94[2]={0xCC, 0x01};
+
+static char write_memory95[2]={0x00, 0xB2};
+static char write_memory96[6]={0xCC, 0x0F,0x0D,0x0B,0x09,0x05};
+
+// cccx
+static char write_memory97[2]={0x00, 0xCA};
+static char write_memory98[2]={0xCC, 0x02};
+
+static char write_memory99[2]={0x00, 0xCC};
+static char write_memory100[4]={0xCC, 0x10,0x0E,0x0C};
+
+// ccdx
+static char write_memory101[2]={0x00, 0xD0};
+static char write_memory102[2]={0xCC, 0x0A};
+
+static char write_memory103[2]={0x00, 0xD1};
+static char write_memory104[2]={0xCC, 0x06};
+//-------------------- sleep out --------------------//
 static char write_memory105[1]={0x11};
+
 static char write_memory106[1]={0x29};
-static char pixels_on[1]={0x23};
 
 static void panel_otm8018b_destroy(struct drm_panel *panel)
 {
@@ -148,13 +328,121 @@ static int otm8018b_prepare(struct drm_panel *panel)
 	ret = panel_otm8018b_power_on(panel_otm8018b);
 	if (ret)
 		return ret;
-
+if(0){	
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory1, sizeof(write_memory1));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory2, sizeof(write_memory2));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory3, sizeof(write_memory3));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory4, sizeof(write_memory4));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory5, sizeof(write_memory5));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory6, sizeof(write_memory6));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory7, sizeof(write_memory7));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory8, sizeof(write_memory8));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory9, sizeof(write_memory9));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory10, sizeof(write_memory10));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory11, sizeof(write_memory11));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory12, sizeof(write_memory12));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory13, sizeof(write_memory13));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory14, sizeof(write_memory14));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory15, sizeof(write_memory15));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory16, sizeof(write_memory16));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory17, sizeof(write_memory17));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory18, sizeof(write_memory18));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory19, sizeof(write_memory19));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory20, sizeof(write_memory20));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory21, sizeof(write_memory21));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory22, sizeof(write_memory22));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory23, sizeof(write_memory23));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory24, sizeof(write_memory24));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory25, sizeof(write_memory25));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory26, sizeof(write_memory26));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory27, sizeof(write_memory27));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory29, sizeof(write_memory29));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory30, sizeof(write_memory30));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory31, sizeof(write_memory31));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory32, sizeof(write_memory32));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory33, sizeof(write_memory33));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory34, sizeof(write_memory34));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory35, sizeof(write_memory35));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory36, sizeof(write_memory36));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory37, sizeof(write_memory37));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory38, sizeof(write_memory38));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory39, sizeof(write_memory39));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory40, sizeof(write_memory40));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory41, sizeof(write_memory41));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory42, sizeof(write_memory42));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory43, sizeof(write_memory43));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory44, sizeof(write_memory44));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory45, sizeof(write_memory45));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory46, sizeof(write_memory46));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory47, sizeof(write_memory47));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory48, sizeof(write_memory48));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory49, sizeof(write_memory49));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory50, sizeof(write_memory50));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory51, sizeof(write_memory51));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory52, sizeof(write_memory52));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory53, sizeof(write_memory53));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory54, sizeof(write_memory54));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory55, sizeof(write_memory55));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory56, sizeof(write_memory56));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory57, sizeof(write_memory57));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory58, sizeof(write_memory58));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory59, sizeof(write_memory59));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory60, sizeof(write_memory60));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory61, sizeof(write_memory61));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory62, sizeof(write_memory62));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory63, sizeof(write_memory63));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory64, sizeof(write_memory64));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory65, sizeof(write_memory65));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory66, sizeof(write_memory66));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory67, sizeof(write_memory67));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory68, sizeof(write_memory68));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory69, sizeof(write_memory69));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory70, sizeof(write_memory70));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory71, sizeof(write_memory71));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory72, sizeof(write_memory72));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory73, sizeof(write_memory73));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory74, sizeof(write_memory74));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory75, sizeof(write_memory75));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory76, sizeof(write_memory76));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory77, sizeof(write_memory77));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory78, sizeof(write_memory78));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory79, sizeof(write_memory79));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory80, sizeof(write_memory80));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory81, sizeof(write_memory81));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory82, sizeof(write_memory82));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory83, sizeof(write_memory83));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory84, sizeof(write_memory84));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory85, sizeof(write_memory85));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory86, sizeof(write_memory86));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory87, sizeof(write_memory87));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory88, sizeof(write_memory88));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory89, sizeof(write_memory89));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory90, sizeof(write_memory90));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory91, sizeof(write_memory91));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory92, sizeof(write_memory92));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory93, sizeof(write_memory93));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory94, sizeof(write_memory94));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory95, sizeof(write_memory95));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory96, sizeof(write_memory96));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory97, sizeof(write_memory97));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory98, sizeof(write_memory98));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory99, sizeof(write_memory99));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory100, sizeof(write_memory100));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory101, sizeof(write_memory101));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory102, sizeof(write_memory102));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory103, sizeof(write_memory103));
+	mipi_dsi_dcs_write_buffer(dsi, &write_memory104, sizeof(write_memory104));
+//	mdelay(250);
+}
 	mipi_dsi_dcs_write_buffer(dsi,&write_memory105, sizeof(write_memory105));
         mdelay(20);
-	mipi_dsi_dcs_write_buffer(dsi,&write_memory106, sizeof(write_memory105));
+	mipi_dsi_dcs_write_buffer(dsi,&write_memory106, sizeof(write_memory106));
         mdelay(5);
+
+if(0){
 	mipi_dsi_dcs_write_buffer(dsi,&pixels_on, sizeof(pixels_on));
         mdelay(5);
+}
 
 	return 0;
 }
@@ -171,7 +459,6 @@ static int otm8018b_unprepare(struct drm_panel *panel)
 	ret = panel_otm8018b_power_off(panel_otm8018b);
 	if (ret)
 		return ret;
-	
 	mipi_dsi_dcs_write_buffer(dsi,&enter_sleep, sizeof(enter_sleep));
 
 	return 0;
@@ -180,39 +467,23 @@ static int otm8018b_unprepare(struct drm_panel *panel)
 static int otm8018b_get_modes(struct drm_panel *panel)
 {
 	struct drm_connector *connector = panel->connector;
-        //struct panel_otm8018b *panel_otm8018b = to_panel_otm8018b(panel);
+        struct panel_otm8018b *panel_otm8018b = to_panel_otm8018b(panel);
 	struct drm_display_mode *mode;
-	u32 hbp, hfp, vbp, vfp, hspw, vspw;
-
+	int ret;
+	
         mode = drm_mode_create(connector->dev);
         if (!mode) {
                 DRM_ERROR("failed to create a new display mode\n");
                 return 0;
         }
 
-	snprintf(mode->name, sizeof(mode->name), "480x864");
+//	snprintf(mode->name, sizeof(mode->name), "480x864");
 
-	mode->clock = 30780;
-
-        hbp = 44;
-        hfp = 46;
-        vbp = 16;
-        vfp = 15;
-        hspw = 4;
-        vspw = 1;
-
-	mode->hdisplay = 480;
-	mode->hsync_start = mode->hdisplay + hfp;
-	mode->hsync_end = mode->hsync_start + hspw;
-	mode->htotal = mode->hsync_end + hbp;
-
-	mode->vdisplay = 864;
-	mode->vsync_start = mode->vdisplay + vfp;
-	mode->vsync_end = mode->vsync_start + vspw;
-	mode->vtotal = mode->vsync_end + vbp;
-
-	mode->flags = 0;
-//	drm_display_mode_from_videomode(&panel_otm8018b->vm, mode);
+	ret = of_get_videomode(panel->dev->of_node, &panel_otm8018b->vm, 0);
+        if (ret < 0)
+                return ret;
+	
+	drm_display_mode_from_videomode(&panel_otm8018b->vm, mode);
         drm_mode_probed_add(connector, mode);
 
 	return 1;
@@ -242,8 +513,6 @@ static int otm8018b_probe(struct mipi_dsi_device *dsi)
         struct panel_otm8018b *panel_otm8018b;
         int ret;
 
-	printk(" vinay otm8018b_probed *********\n");
-
         panel_otm8018b = devm_kzalloc(dev, sizeof(struct panel_otm8018b), GFP_KERNEL);
         if (!panel_otm8018b)
                 return -ENOMEM;
@@ -255,10 +524,10 @@ static int otm8018b_probe(struct mipi_dsi_device *dsi)
         dsi->lanes = 2;
         dsi->format = MIPI_DSI_FMT_RGB888;
         dsi->mode_flags = MIPI_DSI_MODE_VIDEO 
-                | MIPI_DSI_MODE_VIDEO_HFP | MIPI_DSI_MODE_VIDEO_HBP
-                | MIPI_DSI_MODE_VIDEO_HSA ;//| MIPI_DSI_MODE_EOT_PACKET ;
-                //| MIPI_DSI_MODE_VSYNC_FLUSH | MIPI_DSI_MODE_VIDEO_AUTO_VERT;
-
+			| MIPI_DSI_MODE_VIDEO_HBP
+			| MIPI_DSI_MODE_VIDEO_HFP
+			| MIPI_DSI_MODE_VIDEO_HSE 
+			| MIPI_DSI_MODE_EOT_PACKET;
 
         panel_otm8018b->disp_en_gpio = 27;
         ret = gpio_request(panel_otm8018b->disp_en_gpio, "disp_en_gpio");
